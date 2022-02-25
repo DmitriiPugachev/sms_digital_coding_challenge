000f60b3b5fb67dce867fa94c77898cee58ff565
DmitriiPugachev <pugachev.feddebate@gmail.com>
2022-02-25 23:05:56 +0100

    Refactor end date validation.

--- backend/api/v1/validators.py
+++ backend/api/v1/validators.py
@@ -4,8 +4,10 @@
 from rest_framework import validators
 
 
-def end_date_validate(first_value, second_value):
+def end_date_validate(first_value, second_value, first_value_name, second_value_name):
     """Validate the first value is bigger than the second one."""
     if first_value < second_value:
-        raise validators.ValidationError("End date must be bigger than start date.")
+        raise validators.ValidationError(
+            f"{first_value_name} must be bigger than {second_value_name}."
+        )
     return first_value
