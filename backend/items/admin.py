95697a700370153f23af9837aa81d534a0539714
DmitriiPugachev <pugachev.feddebate@gmail.com>
2022-02-25 15:23:46 +0100

    Set up admin config.

--- backend/items/admin.py
+++ backend/items/admin.py
@@ -1,3 +1,28 @@
+"""Items admin config."""
+
+
 from django.contrib import admin
 
-# Register your models here.
+from items.models import Item
+
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = (
+        "city",
+        "start_date",
+        "end_date",
+        "price",
+        "status",
+        "color",
+    )
+    search_fields = ("city",)
+    list_filter = (
+        "start_date",
+        "end_date",
+        "price",
+        "status",
+    )
+    empty_value_display = "-empty-"
+
+
+admin.site.register(Item, ItemAdmin)
