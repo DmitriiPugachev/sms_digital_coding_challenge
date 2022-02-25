2619b5ad6a3e5d07010d85d114ac2f7ae8e34def
DmitriiPugachev <pugachev.feddebate@gmail.com>
2022-02-25 14:35:57 +0100

    Create Item model.

--- backend/items/models.py
+++ backend/items/models.py
@@ -1,3 +1,67 @@
+"""Items models description."""
+
+
+from colorfield.fields import ColorField
 from django.db import models
 
-# Create your models here.
+
+class ItemStatuses:
+    """Constant item statuses."""
+    NEVER = "Never"
+    ONCE = "Once"
+    SELDOM = "Seldom"
+    OFTEN = "Often"
+    DAILY = "Daily"
+    WEEKLY = "Weekly"
+    MONTHLY = "Monthly"
+    YEARLY = "Yearly"
+    ITEM_STATUSES_CHOICES = [
+        (NEVER, NEVER),
+        (ONCE, ONCE),
+        (SELDOM, SELDOM),
+        (OFTEN, OFTEN),
+        (DAILY, DAILY),
+        (WEEKLY, WEEKLY),
+        (MONTHLY, MONTHLY),
+        (YEARLY, YEARLY),
+    ]
+
+
+class Item(models.Model):
+    """Item model description."""
+    city = models.CharField(
+        max_length=200,
+        unique=True,
+        db_index=True,
+        verbose_name="Item city name",
+    )
+    start_date = models.DateField(
+        db_index=True,
+        verbose_name="Item start date",
+    )
+    end_date = models.DateField(
+        db_index=True,
+        verbose_name="Item end date",
+    )
+    price = models.FloatField(
+        verbose_name="Item price",
+    )
+    status = models.CharField(
+        max_length=50,
+        choices=ItemStatuses.ITEM_STATUSES_CHOICES,
+        default=ItemStatuses.NEVER,
+        verbose_name="Item status",
+    )
+    color = ColorField(
+        format="hex",
+        default="#FF0000",
+        verbose_name="Item color",
+    )
+
+    class Meta:
+        verbose_name = "Item"
+        verbose_name_plural = "Items"
+
+    def __str__(self):
+        """Returns string view of a city field."""
+        return self.city
