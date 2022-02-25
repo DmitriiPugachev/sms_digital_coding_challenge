714f6a37523c558c55ffdb868b7321d1e0e47116
DmitriiPugachev <pugachev.feddebate@gmail.com>
2022-02-25 20:56:46 +0100

    Edit main urls.py. Create initial API app structure.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,6 +1,10 @@
+"""Project URLs."""
+
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('api.urls'))
 ]
