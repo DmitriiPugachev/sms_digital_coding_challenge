56225dfafc6d6d1a15549e77d6603021c313d61c
DmitriiPugachev <pugachev.feddebate@gmail.com>
2022-02-26 19:40:54 +0100

    Add DEBUG envinromental variable.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv("SECRET_KEY", default="default_value")
 
-DEBUG = True
+DEBUG = os.getenv("DEBUG", default=False)
 
 ALLOWED_HOSTS = []
 
