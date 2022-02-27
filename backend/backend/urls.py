ac5a7b7f761721bd45ed429b03e073dea0057285
DmitriiPugachev <pugachev.feddebate@gmail.com>
2022-02-28 00:55:28 +0100

    Add redoc docs.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,7 +4,29 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.conf.urls import url
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
+]
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Coding challenge API",
+      default_version='v1',
+      description="Coding challenge project api app documentation",
+      contact=openapi.Contact(email="admin@admin.com"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns += [
+   url(r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0),
+       name="schema-redoc"),
 ]
